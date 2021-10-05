68cbebf9480ce48bb4b0d4e9da9403719ace01c3
Keelan1996 <liverpoolkeelan@gmail.com>
2021-10-05 21:58:47 +0100

    Lab 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { render } from '@testing-library/react';
+import { Header } from './components/header'; // import header
+import { Footer } from './components/footer'; // import footer
+import { Content } from './components/content';// import middle content
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'bootstrap/dist/css/bootstrap.min.css'; // import bootstrap navigation bar
+import { Navbar, Nav } from 'react-bootstrap'; // import nav bar
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; // gives router and switch
+
+// <Router> incases the main code. switch allows to switch between components, 
+//the<Router> gives <route> where you can link your components up and pass the route type you gave in for the nav bar
+// exact is the exact path
+
+// imports class from react called conponent/ this the main component
+class App extends Component {
+
+  // method that takes no arguements
+  render() {
+
+    // headers and time 
+    // header, content and footer being imported
+    // imported bootstrap using nav bar code given on site
+
+    return (
+
+      <Router>
+
+        <div className="App">
+
+          <Navbar bg="primary" variant="dark">
+
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
+            </Nav>
+
+          </Navbar>
+
+          <br />
+
+
+          <Switch>
+
+            <Route path='/' component={Content} exact />
+            <Route path='/read' component={Footer} exact />
+            <Route path='/create' component={Header} exact />
+
+
+          </Switch>
+
+
+
+
+        </div>
+
+      </Router>
+
+    );
+
+  } // render()
+
+}// class app
 
 export default App;
